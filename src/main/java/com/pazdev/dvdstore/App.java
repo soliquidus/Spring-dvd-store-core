10d68f703988f5541fc636c6f580a66ff3733293
Soliquidus <tibo.pfeifer@gmail.com>
2022-01-12 15:11:41 +0100

    Added Spring Boot configuration

--- src/main/java/com/pazdev/dvdstore/App.java
+++ src/main/java/com/pazdev/dvdstore/App.java
@@ -1,18 +1,16 @@
 package com.pazdev.dvdstore;
 
 import com.pazdev.dvdstore.controller.MovieController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 
-@ComponentScan({"com.pazdev.dvdstore.controller", "com.pazdev.dvdstore.service", "com.pazdev.dvdstore.repository.file"})
-@PropertySource("classpath:application.properties")
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class);
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
     }
