aa9db920ea11f5f6d31c9574a26e7c3b20d4a454
Soliquidus <tibo.pfeifer@gmail.com>
2022-01-14 12:29:11 +0100

    Updated repository to auto increment a new movie id

--- src/main/java/com/pazdev/dvdstore/core/repository/file/FileMovieRepository.java
+++ src/main/java/com/pazdev/dvdstore/core/repository/file/FileMovieRepository.java
@@ -17,10 +17,14 @@ public class FileMovieRepository implements MovieRepositoryInterface {
     private File file;
 
     public void addMovie(Movie movie) {
+
+        long lastId = list().stream().map(Movie::getId).max(Long::compare).orElse(0L);
+        movie.setId(lastId+1);
+
         FileWriter writer;
         try {
             writer = new FileWriter(file, true);
-            writer.write(movie.getTitle() + ";" + movie.getGenre() + "\n");
+            writer.write(movie.getId()+";"+movie.getTitle()+";"+movie.getGenre()+";"+movie.getDescription()+"\n");
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/com/pazdev/dvdstore/core/repository/memory/MemoryMovieRepository.java
+++ src/main/java/com/pazdev/dvdstore/core/repository/memory/MemoryMovieRepository.java
@@ -8,10 +8,13 @@ import java.util.List;
 
 //@Repository
 public class MemoryMovieRepository implements MovieRepositoryInterface {
+
+    public static long lastId = 0L;
+
     private static final List<Movie> movies = new ArrayList<>();
 
-
     public void addMovie(Movie movie){
+        movie.setId(lastId++);
         movies.add(movie);
         System.out.println("The movie "+movie.getTitle()+" has been added.");
     }
